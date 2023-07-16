69db6887d0bd6baec5e48687e66c751fb4ae47f0
Andrew Butov <andrew@kali.butov.online>
2023-07-16 22:43:51 +0300

    switch to polling mode

--- src/vk/vk.controller.ts
+++ src/vk/vk.controller.ts
@@ -10,23 +10,19 @@ export class VkController {
   @Post('/')
   @HttpCode(200)
   bot(@Body() cb: any): string {
-    if (cb?.object?.message?.date) {
-      console.log(fromUnixTime(cb.object.message.date))
-      let duration = intervalToDuration({
-        start: fromUnixTime(cb.object.message.date), 
-        end: new Date(),
-      });
+    // if (cb?.object?.message?.date) {
+    //   // console.log(fromUnixTime(cb.object.message.date))
+    //   // let duration = intervalToDuration({
+    //   //   start: fromUnixTime(cb.object.message.date), 
+    //   //   end: new Date(),
+    //   // });
 
-      cb.object.message.age_minutes = duration.minutes;
+    //   // cb.object.message.age_minutes = duration.minutes;
 
-      console.log(
-        // cb,
-        'IN msg ::: ', 
-        `${formatDuration(duration, {
-          delimiter: ', '
-        })} - [${cb.object.message.conversation_message_id}] - [${cb.object.message.from_id}] - [${cb.object.message.text}]`
-      );
-    }
+    // }
+    console.log(
+      'IN msg ::: ', `[${cb.object.message.conversation_message_id}] - [${cb.object.message.from_id}] - [${cb.object.message.text}]`
+    );
     const resp = this.vkService.getBotCB(cb);
     return resp;
   }
