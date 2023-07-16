05cb955536917430fa57decd7c11c23116f3b8d2
Andrew Butov <andrew@kali.butov.online>
2023-07-16 18:59:00 +0300

    switch to polling mode

--- src/vk/vk.service.cb.ts
+++ src/vk/vk.service.cb.ts
@@ -3,7 +3,6 @@ import { ConfigService } from '@nestjs/config';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 import { Timeout } from '@nestjs/schedule';
 import { VkUsersService } from './vk.service.user';
-import { VkMessageService } from './vk.service.message';
 
 const VkBot = require('node-vk-bot-api');
 
@@ -17,7 +16,6 @@ export class VkService {
     private eventEmitter: EventEmitter2, 
     private config: ConfigService,
     private vkUsers: VkUsersService,
-    private vkMessage: VkMessageService,
   ) {
     this.bot = new VkBot({
       token: this.config.get('vk.vkToken'),
@@ -78,8 +76,6 @@ export class VkService {
     }
     // this.logger.log(message, 'VK_S:process message >>>');
     const user = await this.vkUsers.getUserStrById(message.from_id);
-    await this.vkMessage.getUserStrById(message.from_id);
-    
     // this.logger.log(user, 'VK_S:get user info >>>');
     const payload = {
       addedAt: new Date(),
